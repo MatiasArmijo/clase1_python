31c4380c535b2916459f30b70b3a7c168a84716f
j0shua000 <matias.armijo2000@gmail.com>
2023-05-01 16:26:28 -0400

    arduino slave upload

--- arduino_slave/arduino_slave.ino
+++ arduino_slave/arduino_slave.ino
@@ -1,30 +1,54 @@
 #include <Wire.h>
-#define I2C_SLAVE_ADDRESS 11 // 12 pour l'esclave 2 et ainsi de suite
-#define PAYLOAD_SIZE 2
-void setup()
-{
-  Wire.begin(I2C_SLAVE_ADDRESS);
+
+String inputString = "";         // Cadena de texto para guardar la cadena recibida por I2C
+bool stringComplete = false;     // Bandera booleana
+
+int led = 13;
+int direccion=31; //Dirección asignada al ARduino. Asegúrate de que utilices esta dirección en el código de la Raspberry Pi
+ 
+void setup() {
+  pinMode(led, OUTPUT);
+  Wire.begin(direccion);        // Nos unimos al bus i2c bus con la dirección asignada
+  Wire.onReceive(receiveEvent); // Registramos el evento de recepción de datos
   Serial.begin(9600);
-  Serial.println("-------------------------------------I am Slave1");
-  delay(1000);
-  Wire.onRequest(requestEvents);
-  Wire.onReceive(receiveEvents);
+  inputString.reserve(200);
 }
-void loop(){}
-int n = 0;
-void requestEvents()
-{
-  Serial.println(F("---> recieved request"));
-  Serial.print(F("sending value : "));
-  Serial.println(n);
-  Wire.write(n);
+ 
+void loop() {
+  
+  if (stringComplete) {
+    Serial.println("Cadena recibida completa");
+    inputString.remove(0,1);//Removemos 1 carácter. El caracter con index 0, caracter adicional sin utilidad que está mandando Raspberry Pi
+    
+    if (inputString.equals("off") ){
+      digitalWrite(13,0);
+      Serial.println("Apagado");
+    }
+    else if (inputString.equals("on") ){
+      digitalWrite(13,1);
+      Serial.println("Encendido");
+    }
+
+    Serial.println(" ");
+    inputString = "";
+    stringComplete = false;
+  }
+  
 }
-void receiveEvents(int numBytes)
-{
-  Serial.println(F("---> recieved events"));
-  n = Wire.read();
-  Serial.print(numBytes);
-  Serial.println(F("bytes recieved"));
-  Serial.print(F("recieved value : "));
-  Serial.println(n);
+ 
+void receiveEvent(int howMany) {
+  
+
+  while(Wire.available()>0) // Mientras tengamos caracteres en el buffer
+  {
+    char inChar = (char)Wire.read();
+    Serial.print(inChar); 
+    if (inChar == '\n') {
+      stringComplete = true;
+    }
+    else{
+      inputString += inChar;
+    }
+  }
+  
 }
