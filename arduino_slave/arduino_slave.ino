a8d808844f4d6d7f04543b45efdee62eda063e57
j0shua <matias.armijo2000@gmail.com>
2023-05-01 22:27:03 -0400

    Update arduino_slave.ino

--- arduino_slave/arduino_slave.ino
+++ arduino_slave/arduino_slave.ino
@@ -1,12 +1,28 @@
 #include <Wire.h>
 
+#define SLAVE_ADDRESS 0x04
+
+byte data = 0;
+
 void setup() {
-  Wire.begin(); // inicializar la comunicación I2C
+  Serial.begin(9600); // Inicializar el puerto serial
+  Wire.begin(SLAVE_ADDRESS); // Inicializar la comunicación I2C como esclavo
+  Wire.onRequest(requestEvent); // Definir la función para enviar datos por el bus I2C
+  Wire.onReceive(receiveEvent); // Definir la función para recibir datos por el bus I2C
 }
 
 void loop() {
-  Wire.beginTransmission(0x12); // Dirección del dispositivo Python
-  Wire.write(0x01); // enviar un byte de datos
-  Wire.endTransmission(); // terminar la transmisión
-  delay(1000); // esperar un segundo antes de enviar el siguiente dato
+  delay(100);
+}
+
+void requestEvent() {
+  Wire.write(data); // Enviar datos por el bus I2C
+}
+
+void receiveEvent(int byteCount) {
+  if (Wire.available() > 0) {
+    data = Wire.read(); // Leer datos recibidos por el bus I2C
+    Serial.print("Dato recibido: ");
+    Serial.println(data);
+  }
 }
